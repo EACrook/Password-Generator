dc7dc022c09dcc0c374f8408cbb763ad3c23b529
Emily Crook <Emily.A.Crook96@gmail.com>
2021-03-19 21:43:05 -0500

    generate random password works for characterSet

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -2,10 +2,33 @@
 
 var specialChar = [ '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', , ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'];
 var num = [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var upperCase = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-var lowerCase = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'] 
+var upperCase = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var lowerCase = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']; 
 
 
+// --Prompt Length of password-- 
+var passwordLength = 10
+
+// --character preferences
+  // upper and lower case letter
+var characterSet = upperCase.concat(lowerCase)
+
+// --implement generate password function
+function generatePassword() {
+
+  var password = '';
+
+  for (var i = 0; i < passwordLength; i++) {
+      var randomNum = getRandomInt(0, characterSet.length - 1)
+       password = password + characterSet[randomNum];  
+  }
+  return password;
+}
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
